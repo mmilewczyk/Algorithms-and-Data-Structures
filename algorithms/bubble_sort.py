2e62d81662e55d3dd690eacd107c26f0df785ae6
Mateusz Milewczyk <54153910+agiklo@users.noreply.github.com>
2021-01-13 19:12:06 +0100

    Add bubble_sort implementation in python

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,12 @@
+tab = [8, 2, 1, 9, 5]
+print(tab)
+
+i = 0
+while i < len(tab) - 1:
+    j = 0
+    while j < len(tab) - 1 - i:
+        if tab[j] > tab[j + 1]:
+            tab[j], tab[j + 1] = tab[j + 1], tab[j]
+        j += 1
+    i += 1
+    print(tab)
